2cd33ee35787cea77deac3e739463fbf5127fba0
Test User <sitedeneme20@gmail.com>
2020-03-25 16:13:30 +0200

    end

--- app.js
+++ app.js
@@ -2,5 +2,22 @@ const http = require("http");
 
 http.createServer((request , response) => {
 	response.write("Hello World");
+	switch(request.url){
+		case "/":
+		response.write("MAIN PAGE")
+		break;
+
+		case "/about":
+		response.write("ABOUT PAGE")
+		break;
+
+		case "/contact":
+		response.write("CONTACT PAGE")
+		break;
+
+		default:
+		response.write("PAGE NOT FOUND")
+
+	}
 	response.end();
 }).listen(process.env.PORT)
